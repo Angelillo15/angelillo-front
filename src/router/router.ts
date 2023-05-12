768c38345c3839f6baf1c518b00c014506e558fc
Angelillo15 <contact@angelillo15.es>
2023-05-12 11:39:43 +0000

    Added Resource Page

--- src/router/router.ts
+++ src/router/router.ts
@@ -5,6 +5,11 @@ const routes: RouteRecordRaw[] = [
         path: "/",
         name: "Home",
         component: () => import("../views/Home.vue")
+    },
+    {
+        path: "/resources",
+        name: "Resources",
+        component: () => import("../views/Resources.vue")
     }
 ];
 
---
+++ src/views/Resources.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Resources page</h1>
+</template>
+
+<script lang="ts">
+export default {
+    name: "Resources"
+}
+</script>
