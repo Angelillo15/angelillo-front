1d642ddfd11fa83da7b6b57aac73d709b49a923c
Angelillo15 <contact@angelillo15.es>
2023-05-13 14:16:23 +0200

    Added Resource page

---
+++ src/components/Resource.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="resource">
+        <div class="resource__image">
+            <img :src="image" :alt="name" />
+        </div>
+        <div class="resource__content">
+            <h3 class="resource__name">{{ name }}</h3>
+            <p class="resource__description">{{ description }}</p>
+            <a :href="url" target="_blank" class="resource__link">Visit</a>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Resource',
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        url: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+    },
+});
+</script>
+
+<style scoped>
+.resource {
+    background-color: var(--card-bg);
+    margin: 10px;
+    padding: 1rem;
+    border-radius: 3%;
+    min-height: 350px;
+    max-height: 500px;
+    color: var(--sidebar-button-color);
+}
+
+.resource:hover {
+    transition: filter 0.2s ease-in-out;
+    filter: drop-shadow(0 0 10px #3a3a3a);
+}
+
+.resource__image {
+    width: 100%;
+    height: 10rem;
+    margin-bottom: 1rem;
+    border-radius: 0.5rem;
+    overflow: hidden;
+}
+
+.resource__image img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+.resource__content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+}
+
+.resource__name {
+    margin-bottom: 0.5rem;
+    font-size: 1.25rem;
+    font-weight: 600;
+}
+
+.resource__description {
+    margin-bottom: 1rem;
+    font-size: 1rem;
+    font-weight: 400;
+}
+
+.resource__link {
+    padding: 0.5rem 1rem;
+    border-radius: 0.25rem;
+    background-color: #000;
+    color: #fff;
+    text-decoration: none;
+    transition: all 0.2s ease-in-out;
+}
+
+.resource__link:hover {
+    background-color: var(--nookure);
+    color: #fff;
+}
+
+
+</style>
--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -1,9 +1,55 @@
 <template>
     <h1>Resources page</h1>
+    <div class="grid-row">
+        <Resource
+            name="MAStaff"
+            url="https://builtbybit.com/resources/25460/"
+            description="Staff Plugin for Spigot, Bukkit, Paper, Bungee and Folia"
+            image="../../public/resources/mastaff.png"
+        />
+
+        <Resource
+            name="Ice Minecraft Theme"
+            url="https://builtbybit.com/resources/26274/"
+            description="A Ice theme based on Minecraft for Pterodactyl Panel"
+            image="../../public/resources/ice.png"
+        />
+
+        <Resource
+            name="Minecraft Purple Theme"
+            url="https://pterodactyl-resources.com/resources/resource/43/"
+            description="An Purple theme based on Minecraft for Pterodactyl Panel"
+            image="../../public/resources/purple.png"
+        />
+
+    </div>
+    <div class="grid-row"></div>
 </template>
 
 <script lang="ts">
-export default {
-    name: "Resources"
-}
+import { defineComponent } from 'vue';
+import Resource from '../components/Resource.vue';
+
+export default defineComponent({
+    name: 'Resources',
+    components: {
+        Resource,
+    },
+});
 </script>
+
+<style scoped>
+.grid-row {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(20px, 400px));
+    grid-gap: 10px 0px;
+    justify-content: center;
+    align-items: baseline;
+    text-align: center;
+}
+
+.container {
+    max-width: 400px;
+    margin: auto;
+}
+</style>
